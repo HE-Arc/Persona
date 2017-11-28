f366ad292a9b81469d8e986899261bfec97c78d3
Christophe Hirschi <christophe.hirschi@he-arc.ch>
2017-11-28 16:24:22 +0100

    Déploiement fix #5

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,9 +21,9 @@ class CreateUsersTable extends Migration
             $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('country_id')->unsigned()->unique();
+            $table->integer('country_id')->unsigned();
             $table->foreign('country_id')->references('id')->on('countries')->onDelete('SET NULL');
-            $table->integer('personality_id')->index()->unsigned();
+            $table->integer('personality_id')->unsigned();
             $table->foreign('personality_id')->references('id')->on('personalities')->onDelete('SET NULL');
             //$table->string('description');
             $table->char('gender', 1);

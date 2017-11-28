071fb1ef76243c43ed95471d17cb5ebdc37f12c1
Christophe Hirschi <christophe.hirschi@he-arc.ch>
2017-11-28 16:12:13 +0100

    Déploiement fix #3
    On test, on test...

--- database/migrations/2017_10_04_143259_create_countries_table.php
+++ database/migrations/2017_10_04_143259_create_countries_table.php
@@ -14,6 +14,7 @@ class CreateCountriesTable extends Migration
     public function up()
     {
         Schema::create('countries', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->char('initals', 2);
             $table->string('name');

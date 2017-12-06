e578ffa3e2fc7d5203e43151ba1f60920df848aa
Christophe Hirschi <christophe.hirschi@he-arc.ch>
2017-12-06 13:03:36 +0100

    Fix problème de seeding avec ce fichier SQL

--- resources/assets/sql/SqlQualities.sql
+++ resources/assets/sql/SqlQualities.sql
@@ -1,30 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 10 oct. 2017 à 08:37
--- Version du serveur :  5.7.19
--- Version de PHP :  7.1.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `bdd_persona`
---
-
--- --------------------------------------------------------
-
-
 INSERT INTO `qualities` (`id`, `quality`, `created_at`, `updated_at`) VALUES
 (1, 'Adventurous', NOW(), NOW()),
 (2, 'Ambitious', NOW(), NOW()),
@@ -76,8 +49,3 @@ INSERT INTO `qualities` (`id`, `quality`, `created_at`, `updated_at`) VALUES
 (48, 'Responsible', NOW(), NOW()),
 (49, 'Talkative', NOW(), NOW()),
 (50, 'Technological', NOW(), NOW());
-
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
